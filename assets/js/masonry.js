72ce99c87942a0c0a51a998c62e8dc1b04071f65
Jo-a-n <56139906+Jo-a-n@users.noreply.github.com>
2023-07-16 16:47:55 +0000

    Deploying to gh-pages from @ Jo-a-n/Jo-a-n.github.io@73030aee703933984954a7eafdb0fcd5dc025deb 🚀

--- assets/js/masonry.js
+++ assets/js/masonry.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   // Init Masonry
   var $grid = $('.grid').masonry({
-    gutter: 10,
+    gutter: 24,
     horizontalOrder: true,
     itemSelector: '.grid-item',
   });
