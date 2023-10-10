4e2011b58a568488c733cb7cd05baa4362f58e9a
Jo-a-n <56139906+Jo-a-n@users.noreply.github.com>
2023-10-10 20:55:50 +0300

    Add homepage horizontal slider & hover animations

--- assets/js/common.js
+++ assets/js/common.js
@@ -28,10 +28,50 @@ $(document).ready(function() {
     $(document).scroll(function(){
         if ($(document).scrollTop() > 240) {
             $(".bg-image-holder").css({ opacity: '0.02' })
-            $("body").css({ backgroundColor: '#F5FFFE' })
+            $("body").css({ backgroundColor: 'rgb(244, 244, 244)' })
         } else {
             $(".bg-image-holder").css({ opacity: '.25' })
             $("body").css({ backgroundColor: 'var(--global-bg-color)' })
         }
     })
 })
+
+//Get the gallery from th DOM
+const gallery = document.querySelector(".gallery");
+const gallerywrapper = document.querySelector(".gallery-wrapper");
+//Get the gallery total width
+let galleryWidth = gallery.offsetWidth;
+let gallerywrapperWidth = gallerywrapper.offsetWidth;
+/*Get the ammount to scroll horizontally 
+by subtracting the window width 
+from the full width of the gallery*/
+let ammountToScroll = galleryWidth - gallerywrapperWidth;
+console.log(ammountToScroll)
+
+
+ScrollTrigger.create({
+    trigger: ".gallery-wrapper",
+    start: "top 220px",
+    pin: true,
+    onUpdate: (self) => {
+        // console.log(
+        //   "progress:",
+        //   self.progress.toFixed(3),
+        //   "direction:",
+        //   self.direction,
+        //   "velocity",
+        //   self.getVelocity()
+        // );
+        $('.gallery').css({transform: `translate(-${self.progress.toFixed(3) * ammountToScroll}px)`})
+      },
+})
+
+//Initialize Lenis smooth scrolling
+const lenis = new Lenis();
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
