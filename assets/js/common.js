490a50c733676580ec01b68916a04fdb3024b8c4
Jo-a-n <56139906+Jo-a-n@users.noreply.github.com>
2023-10-22 16:18:36 +0000

    Deploying to gh-pages from @ Jo-a-n/Jo-a-n.github.io@6fe7dd5d4272d7cd05bf27890aae5c75471c8e15 🚀

--- assets/js/common.js
+++ assets/js/common.js
@@ -23,3 +23,66 @@ $(document).ready(function () {
     }
 });
 
+// scroll hide background image
+$(document).ready(function() {
+    $(document).scroll(function(){
+        if ($(document).scrollTop() > 360) {
+            $(".bg-image-holder").css({ opacity: '0.02' })
+            $("body").css({ backgroundColor: 'var(--scroll-bg-color)' })
+        } else {
+            $(".bg-image-holder").css({ opacity: 'var(--bg-opacity)' })
+            $("body").css({ backgroundColor: 'var(--global-bg-color)' })
+        }
+    })
+})
+
+
+//Horizontal scroll on homepage
+
+//Get the gallery from th DOM
+const gallery = document.querySelector(".gallery");
+const gallerywrapper = document.querySelector(".gallery-wrapper");
+//Get the gallery total width
+let galleryWidth = gallery.offsetWidth;
+let gallerywrapperWidth = gallerywrapper.offsetWidth;
+//This is a static var where I put the padding-right found in gallery
+let paddingright = 16;
+/*Get the ammount to scroll horizontally 
+by subtracting the window width 
+from the full width of the gallery*/
+let ammountToScroll = galleryWidth - gallerywrapperWidth - paddingright;
+console.log(ammountToScroll)
+
+gsap.registerPlugin(ScrollTrigger);
+
+//responsive
+let mm = gsap.matchMedia();
+
+mm.add("(min-width: 560px)", () => {
+    ScrollTrigger.create({
+        trigger: ".gallery-wrapper",
+        start: "top 220px",
+        pin: true,
+        onUpdate: (self) => {
+            // console.log(
+            //   "progress:",
+            //   self.progress.toFixed(3),
+            //   "direction:",
+            //   self.direction,
+            //   "velocity",
+            //   self.getVelocity()
+            // );
+            $('.gallery').css({transform: `translate(-${self.progress.toFixed(3) * ammountToScroll}px)`})
+        },
+    });
+});
+
+//Initialize Lenis smooth scrolling
+const lenis = new Lenis();
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,8 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const mode_toggle = document.getElementById("light-toggle");
+    const mode_toggles = document.querySelectorAll(".light-toggle");
 
-    mode_toggle.addEventListener("click", function() {
-        toggleTheme(localStorage.getItem("theme"));
+    mode_toggles.forEach(function(mode_toggle) {
+        mode_toggle.addEventListener("click", function() {
+            toggleTheme(localStorage.getItem("theme"));
+        });
     });
 });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const mode_toggle = document.getElementById("light-toggle");
+
+//     mode_toggle.addEventListener("click", function() {
+//         toggleTheme(localStorage.getItem("theme"));
+//     });
+// });
 
